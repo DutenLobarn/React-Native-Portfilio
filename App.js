c873062c0f8d88049572ce6bcb99266261f6af3c
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2022-01-12 22:12:03 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,24 +4,26 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={h1.h1}>Mandus portfolio snygg</Text>
+      <Text style={styles.h1}>Mandus portfolio Nice</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 
-const h1 = StyleSheet.create({
-  h1: {
-    fontSize: 20,
-  },
-});
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#000",
+
+  },
+  h1: {
+    fontSize: 35,
+    color: "#D02323",
+    textAlign: "center",
+    paddingTop: 100,
   },
 });
