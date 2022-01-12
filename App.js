690069c23eeda09d6132a2a7176c2e94a168e93b
Cuong1979 <70330972+cuong1979@users.noreply.github.com>
2022-01-12 15:11:26 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mandus portfolio22</Text>
+      <Text>Mandus portfolio</Text>
       <StatusBar style="auto" />
     </View>
   );
